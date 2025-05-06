50ce5aec07842e782cc5e9ca8d006f377c1f5bd8
inkAja <mdghifari17@gmail.com>
2025-05-06 15:34:05 +0700

    change

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -17,5 +17,5 @@ const classes = computed(() => {
 <template>
     <div class="w-fit px-3 py-2 bg-light-green text-black-green rounded-sm cursor-pointer hover:bg-black hover:text-light-green transition">
         <slot></slot>
-    </button>
+    </div>
 </template>
